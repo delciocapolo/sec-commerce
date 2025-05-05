60913c12431cfe8f03753e84f83dbc131cebffee
delciocapolo <delciocapolo096@gmail.com>
2025-05-05 14:30:49 +0100

    feat: configs done

--- src/api/config/database.php
+++ src/api/config/database.php
@@ -1,13 +1,13 @@
 <?php
-$connection_config = "mysql:host={$_ENV['DATABASE_HOST']};dbname={$_ENV['DATABASE_NAME']}";
+$dsn = "mysql:host={$_ENV['DATABASE_HOST']};port={$_ENV["DATABASE_PORT"]};dbname={$_ENV['DATABASE_NAME']}";
 
 try {
    $database_connection = new PDO(
-      $connection_config,
+      $dsn,
       $_ENV['DATABASE_USER'],
       $_ENV['DATABASE_PASSWORD']
    );
-   echo "connection successfully";
+   $database_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (\Throwable $error) {
    throw new Exception($error->getMessage());
 }
