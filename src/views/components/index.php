07cde5d74cc65d372140a82e4b79ca81a472e500
delciocapolo <delciocapolo096@gmail.com>
2025-05-24 14:37:42 +0100

    feat: add autoload view components

--- src/views/components/index.php
+++ src/views/components/index.php
@@ -1,4 +1,35 @@
 <?php
-require_once BASE_PATH . "/src/views/components/start-rating/startRating.php";
-require_once BASE_PATH . "/src/views/components/card-product/index.php";
-require_once BASE_PATH . "/src/views/components/title/index.php";
+function handlePathComponents(string $BASE_PATH)
+{
+   try {
+      $componentsPath = $BASE_PATH . "/src/views/components/";
+
+      // check is directory exists
+      if (!is_dir($componentsPath)) {
+         throw new Exception("Diretório de componentes não encontrado: " . $componentsPath);
+      }
+
+      // get all entries in the components directory
+      $entries = scandir($componentsPath);
+
+      // filter out the entries to get only directories
+      $directories = array_filter($entries, function ($entry) use ($componentsPath) {
+         return $entry !== '.' && $entry !== '..' && is_dir($componentsPath . $entry);
+      });
+
+      // reindex the array to have a clean numeric index
+      $directories = array_values($directories);
+
+      foreach ($directories as $component) {
+         $componentPath = BASE_PATH . "/src/views/components/{$component}/index.php";
+
+         if (file_exists($componentPath)) {
+            require_once $componentPath;
+         }
+      }
+   } catch (\Throwable $th) {
+      die("Erro ao carregar componentes: " . $th->getMessage());
+   }
+}
+
+handlePathComponents(BASE_PATH);
